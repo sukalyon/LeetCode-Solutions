16eb4d52961b9c2cbd47b92df177077630e6ee1a
Su Kalyon <136194864+sukalyon@users.noreply.github.com>
2026-08-23 02:26:26 +0300

    Time: 1 ms (43.22%), Space: 55.3 MB (47.46%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.ts
@@ -0,0 +1,13 @@
+function numIdenticalPairs(nums: number[]): number {
+let count: number =0;
+
+ for (let i =0; i<nums.length; i++){
+  for(let j=i+1; j<nums.length; j++){
+    if (nums[i]===nums[j]) {
+      count++
+    }
+  }
+
+ }
+ return count;    
+};
